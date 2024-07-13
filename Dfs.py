5982e363e7306492054b287e85a705f2675baf45
pujitha3perambaduru <pujithaperambaduru2005@gmail.com>
2024-07-13 15:23:29 +0530

    Update Dfs.py

--- Dfs.py
+++ Dfs.py
@@ -1,1 +1,21 @@
+def dfs(graph, start):
+    visited = set()  
+    def dfs_code(node):
+        visited.add(node)
+        print(f"Node: {node}")
+        print(f"  Edges: {graph[node]}")
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                dfs_code(neighbor)
+    dfs_code(start)
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+start_node = 'A'
+dfs(graph, start_node)
 
