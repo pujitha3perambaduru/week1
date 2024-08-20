1d49a275bc6ee3c3f666415795bb0e6ba9a4b9d9
pujitha3perambaduru <pujithaperambaduru2005@gmail.com>
2024-08-20 20:32:04 +0530

    Create plotting.py

---
+++ plotting.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+x=[1,20,27,35,40,45,50]
+y=[1,1,6,160,161,162,206]
+z=[0,10,40,65,89,160,200]
+plt.plot(x,y,marker='*',color='red')
+plt.plot(y,z,marker='*',color='blue')
+plt.show()
