c23e5b5a85d314932339cf653ff379cba41b071c
pujitha3perambaduru <pujithaperambaduru2005@gmail.com>
2024-08-22 14:16:40 +0530

    Update pie_plot.py

--- pie_plot.py
+++ pie_plot.py
@@ -1,8 +1,12 @@
-from matplotlib import pyplot as plt
-import numpy as np
-cars = ['AUDI', 'BMW', 'FORD',
-        'toyota', 'HONDA', 'suzuki']
-data = [20, 15, 45, 10, 39, 17]
-fig = plt.figure(figsize=(10, 3))
-plt.pie(data, labels=cars)
+import matplotlib.pyplot as plt
+india = [1, 20, 27, 35, 40, 45, 50]
+srilanka = [1, 1, 6, 160, 161, 162, 206]
+overs = [0, 10, 40, 65, 89, 160, 200]
+plt.figure(figsize=(10, 5)) 
+plt.subplot(1, 2, 1)
+plt.pie(india, labels=overs, startangle=140, radius=0.7)
+plt.title('India Performance')
+plt.subplot(1, 2, 2)
+plt.pie(srilanka, labels=overs, startangle=140, radius=0.7) 
+plt.title('Sri Lanka Performance')
 plt.show()
